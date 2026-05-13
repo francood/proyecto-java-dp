cb885274cdf748a86136a675434c3b6af05b4d1f
Elizabet <Elizabet@192.168.1.3>
2026-05-12 22:25:57 -0500

    Tarea B2 completada

--- proyecto-dp/src/main/java/productos/MenuPersonalizado.java
+++ proyecto-dp/src/main/java/productos/MenuPersonalizado.java
@@ -1,9 +1,45 @@
 package productos;
-public class MenuPersonalizado extends Producto{
 
-    public MenuPersonalizado(String nombre, double precioBase) {
-        super(nombre, precioBase);
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+
+*/
+public class MenuPersonalizado implements ProductoVendible {
+
+    private ProductoVendible base;
+    private List<String> nombresExtras;
+    private List<Double> costosExtras;
+    private List<String> ingredientesQuitados;
+
+    public MenuPersonalizado(ProductoVendible base) {
+        this.base = base;
+        this.nombresExtras = new ArrayList<>();
+        this.costosExtras = new ArrayList<>();
+        this.ingredientesQuitados = new ArrayList<>();
     }
 
-    //falta
+    public void agregarExtra(String nombre, double costo) {
+        nombresExtras.add(nombre);
+        costosExtras.add(costo);
+    }
+
+    public void quitarIngrediente(String ingrediente) {
+        ingredientesQuitados.add(ingrediente);
+    }
+    
+    @Override
+    public String getNombre() {
+     return base.getNombre() + " personalizado";
+    }
+
+    @Override
+    public double getPrecio() {
+        double total = base.getPrecio();
+        for(double costo: costosExtras){
+        total= total+costo;
+        }
+    return total;
+    }
 }
--- proyecto-dp/src/main/java/restaurante/Restaurante.java
+++ proyecto-dp/src/main/java/restaurante/Restaurante.java
@@ -4,6 +4,7 @@ import productos.Plato;
 import productos.Bebida;
 import productos.Combo;
 import productos.Adicionable;
+import productos.MenuPersonalizado;
 import productos.Producto;
 import productos.ProductoVendible;
 
@@ -11,26 +12,35 @@ public class Restaurante {
 
     public static void main(String[] args) {
 
-        // 1.Crear productos
+        // Crear productos
         Plato plato1 = new Plato("Aeropuerto", 22.5);
         Plato plato2 = new Plato("Tallarines verdes", 18);
-        Producto pizza = new Plato("Pizza Americana", 23);
+        ProductoVendible pizza = new Plato("Pizza Americana", 23);
         Bebida bebida1 = new Bebida("Limonada", 4.5, "mediano");
         Adicionable quesoExtra = new Adicionable(plato1, "Queso extra", 5);
         System.out.println("plato 1: " + quesoExtra.getNombre());
 
-        // 2.Crear combo con descuento 15%
+        // Crear combo con descuento 15%
         Combo combo1 = new Combo("Combo criollo", 15);
         combo1.agregarProducto(plato1);
         combo1.agregarProducto(plato2);
         combo1.agregarProducto(bebida1);
         System.out.println("Precio del combo: " + combo1.getPrecio());
-
-        // 3.Crear pedido y sucursal
+        
+        //Crear MenuPersonalizado
+        MenuPersonalizado menu = new MenuPersonalizado(pizza);
+        menu.agregarExtra("Tomate", 5);
+        menu.quitarIngrediente("cebolla");
+        menu.getPrecio();
+        
+        
+        System.out.println("Menu Personalizado: "+menu.getNombre());
+        System.out.println("Precio del menu personalizado: "+menu.getPrecio());
+        // Crear pedido y sucursal
         Cliente cliente1 = new Cliente("C1001", "Ana Rojas");
         Restaurante sucursal = new Restaurante();
 
-        //4. Crear Pedido
+        //Crear Pedido
         Pedido pedido1 = new Pedido("PED-412", TipoCanal.PARA_LLEVAR, cliente1, sucursal);
 
         //Para llevar
@@ -39,7 +49,7 @@ public class Restaurante {
         CanalProcesador procesar = new ParaLlevar();
         procesar.procesar(pedido1);
 
-        // 5.Crear ítems y agregar al pedido
+        //Crear ítems y agregar al pedido
         ItemPedido item1 = new ItemPedido(plato1, 1);
         ItemPedido item2 = new ItemPedido(plato2, 3);
         ItemPedido item3 = new ItemPedido(pizza, 2);
@@ -47,7 +57,7 @@ public class Restaurante {
         pedido1.agregarItem(item2);
         pedido1.agregarItem(item3);
 
-        //6. Cambio de estados
+        //Cambio de estados
         try {
             pedido1.confirmarPedido();
             pedido1.enPreparacion();
@@ -60,11 +70,11 @@ public class Restaurante {
         plato1.añadirExtras("Brocoli");
         plato1.quitarIngredientes("cebolla");
 
-        // 5.Aplicar promoción del 5% sobre el total del pedido
+        // Aplicar promoción del 5% sobre el total del pedido
         PromocionesTemporada promo1 = new PromocionesTemporada("Jueves de promo", 5);
         double totalConDescuento = promo1.aplicarPromocion(pedido1.getItems());
 
-        // 6.Mostrar resultados
+        // Mostrar resultados
         System.out.println("Subtotal sin descuento: " + pedido1.total());
         System.out.println("Total con descuento: " + totalConDescuento);
         System.out.println("Ahorro: " + (pedido1.total() - totalConDescuento));
